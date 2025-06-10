03bd95ef7423aaee9fb4f4f5929789305cb3ed3d
kebussofia <sofikebus@gmail.com>
2025-06-10 12:08:38 +0300

    Build main page structure, add navigation

--- js/swiper-init.js
+++ js/swiper-init.js
@@ -23,3 +23,33 @@ const swiper_new = new Swiper('#swiper-01', {
 //   },
 
 });
+
+const swiper_new2 = new Swiper('#swiper-02', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  centerInsufficientSlides: true,
+  centeredSlides: true,
+  centeredSlidesBounds: true,
+
+  // allowSlideNext: false,
+  // allowSlidePrev: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+// And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+
+});
